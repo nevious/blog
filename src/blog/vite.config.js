32ebe7ed14dd8fc2df9902dcdcb334f4fd1c1101
Christian Schläppi <christian.schlaeppi@codedump.ch>
2026-09-20 13:14:43 +0200

    added sitemap.xml

--- src/blog/vite.config.js
+++ src/blog/vite.config.js
@@ -3,12 +3,23 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import Sitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    Sitemap({
+      hostname: 'https://nevi-blog.netlify.app',
+      dynamicRoutes: [
+        '/posts/scotland-2026',
+        '/posts/i-keep-forgetting-shit',
+        '/posts/belpberg-august-26',
+        '/posts/interesting-things',
+        '/posts/initial'
+      ]
+    })
   ],
   resolve: {
     alias: {
